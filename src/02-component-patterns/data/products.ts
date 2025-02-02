a5a7b2034c3da78c9d72d76a99f4b8c270baaedc
Victor Nunez <victoralejandronunezt@gmail.com>
2025-02-01 23:17:29 -0300

    Limpieza

---
+++ src/02-component-patterns/data/products.ts
@@ -0,0 +1,8 @@
+export const products = 
+[
+    {
+        id: '12',
+        title: 'Coffee Mug - Card',
+        img: './coffee-mug.png'
+    }
+]
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,29 +1,14 @@
 
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
 
-import { useShoppingCart } from '../hooks/useShoppingCart';
-import { Product, ProductCardHOCProps } from '../interfaces/interfaces';
-
 import '../styles/custom-styles.css';
 
-const product = {
-    id: '12',
-    title: 'Coffee Mug - Card',
-    img: './coffee-mug.png'
-}
+import { products } from "../data/products"
 
-// const product2 = {
-//     id: '22',
-//     title: 'Coffee Mug - Meme',
-//     img: './coffee-mug2.png'
-// }
-
-// const products: ProductCardHOCProps[] = [ product, product2 ]
+const product = products[0]
 
 export const ShoppingPage = () => {
     
-    // const { products, shoppingCart, onProductCountChange } = useShoppingCart()
-
     return (
         <div>
             <h1>Shopping Store</h1>
@@ -32,30 +17,18 @@ export const ShoppingPage = () => {
             <ProductCard
                 key={product.id}
                 product={ product }
-                className="bg-dark text-white"
                 initialValues = {{ 
-                    count: 4,
-                    maxCount: 11,
+                    count: 0,
+                    // maxCount: 11,
                 }}
                 >
 
                 {
-                    ( args ) => (
+                    ({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
                         <>
-                            <ProductImage className="custom-image" />
-                            <ProductTitle className="text-bold"/>
-                            <ProductButtons className="custom-buttons" />
-
-                            <button onClick={() => args.increaseBy(-2)}>-2</button>
-                            <button onClick={ args.reset }>reset</button>
-                            {
-                                (!args.isMaxCountReached && <button onClick={() => args.increaseBy(2)}>+2</button>)
-                            }
-                            <span style={{ paddingLeft: '4px' }}>{args.count}</span>
-                            <span style={{ paddingLeft: '4px', paddingRight: '4px' }}>|</span>
-                            <span>{args.maxCount}</span>
-
-                            {/* <span>{JSON.stringify(args, null, 3)}</span> */}
+                            <ProductImage/>
+                            <ProductTitle/>
+                            <ProductButtons/>
                         </>
                     )
                 }
